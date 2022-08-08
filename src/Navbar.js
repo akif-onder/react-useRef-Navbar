b32c1db54e8215ac3e836daa77eb4b068a5bc7cc
akif-onder <akifonder@gmail.com>
2022-08-08 16:30:33 +0300

    toggle

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,45 +4,40 @@ import { links, social } from './data'
 import logo from './logo.svg'
 
 const Navbar = () => {
+  const [showLinks, setShowLinks] = useState(false);
   return <nav>
     <div className='nav-center '>
       <div className='nav-header'>
         <img src={logo} alt='logo' />
-        <button className='nav-toggle'>
+        <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
           <FaBars />
         </button>
       </div>
-      <div className='links-container show-container'>
+      <div className={`${showLinks ? 'links-container show-container' : 'links-container'}`}>
         <ul className='links'>
-          <li>
-            <a href='#'>home</a>
-          </li>
-          <li>
-            <a href='#'>about</a>
-          </li>
-          <li>
-            <a href='#'>contact</a>
-          </li>
-          <li>
-            <a href='#'>products</a>
-          </li>
-
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            )
+          })}
         </ul>
       </div>
-      <ul className='social-icons'>
-        <li>
-          <a href='https://www.twitter.com'><FaTwitter /></a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'><FaFacebook /></a>
-        </li>
-        <li>
-          <a href='https://www.twitter.com'><FaInstagram /></a>
-        </li>
-      </ul>
-    </div>
+    <ul className='social-icons'>
+      {social.map((soc) => {
+        const { id, url, icon } = soc;
+        return (
+          <li key={id}>
+            <a href={url}>{icon}</a>
+          </li>
+        )
+      })}
+    </ul>
+  </div>
 
-  </nav>
+  </nav >
 }
 
 export default Navbar
